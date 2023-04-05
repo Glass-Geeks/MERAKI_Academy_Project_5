194d6e6ef7e0213f1cf21af598c5fe9972bbbd2a
alhajjkhalid <khaled.alhajj@gmail.com>
2023-04-05 23:36:03 +0300

    Added starter code

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-require('dotenv').config();
-const app = express()
-const db = require('./models/db')
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+// Handles any other endpoints [unassigned - endpoints]
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
