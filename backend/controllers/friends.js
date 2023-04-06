5b36a156c11bb6716051485fba8ba76698edf7ac
AhmedAlSawalmeh <sawalmeh.ahmed@gmail.com>
2023-04-07 00:24:04 +0300

    start with frinds controller

--- backend/controllers/friends.js
+++ backend/controllers/friends.js
@@ -1,14 +1,38 @@
 const { pool } = require("../module/db");
 
 const createFriendConnection = async (req, res) => {
-  const { user_id } = req.params;
-  const { someData } = req.body;
-  const QUERY = ``;
-  const VALUE = [];
+  const userID = req.params.id
+  
+  const {user_id ,friend_id,status } = req.body;
+  const VALUE = [userID,friend_id,status];
+  const QUERY = `INSERT INTO connection (user_id,friend_id,status) VALUES($1,$2,$3) RETURNING *`;
+  try{
+   const response= await pool.query(QUERY,VALUE);
+   res.status(201).json({
+    success :true,
+    Message:"Connection Send",
+    connection:response.rows
+   })
+  }
+  catch(err){
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      err: err.message
+    })
+  }
 };
 const getAllFriends = async (req, res) => {
-  const { id } = req.params;
-  const QUERY = ``;
+  const { id } = req.params.id;
+  const QUERY = `SELECT  `;
 };
 
-module.exports = { createFriendConnection, getAllFriends };
+const getFriendRequests = (req,res)=>{
+
+}
+
+const answerFriendRequest = (req,res)=>{
+
+}
+
+module.exports = { createFriendConnection, getAllFriends ,getFriendRequests,answerFriendRequest};
--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -2,9 +2,14 @@ const express = require("express");
 const friendsRouter = express.Router();
 const {
   createFriendConnection,
-  getAllFriends,
+  getAllFriends,getFriendRequests,answerFriendRequest
 } = require("../controllers/friends");
 
 friendsRouter.post("/", createFriendConnection);
+friendsRouter.get("/requests/:id",getFriendRequests)
+friendsRouter.put("/requsts/:id/answer",answerFriendRequest)
 friendsRouter.get("/:id", getAllFriends);
+
+
+
 module.exports = friendsRouter;
