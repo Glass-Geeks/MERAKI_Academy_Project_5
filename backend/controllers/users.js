f3857394b91fbac159068f095faa72f9afe3b907
alhajjkhalid <khaled.alhajj@gmail.com>
2023-04-06 22:18:22 +0300

    Added login controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -38,7 +38,6 @@ const register = async (req, res) => {
     });
 };
 
-
 // Generate Token Function
 
 /* 
@@ -53,13 +52,54 @@ const generateToken = (whatEver Value You Need In The PayLoad "user_Id ...") => 
   };
   return jwt.sign(payload, SECRET, options);
 }; */
-const login = async (req, res) => {
-  const QUERY = ``;
-  const VALUE = [];
-  const { someData } = req.body;
-  //   after login successfully invoke the token func generateToken(user_Id ...)
-};
+const login = (req, res) => {
+  const { email, password } = req.body;
 
+  const query = `SELECT * FROM users WHERE email = $1`;
+  const data = [email.toLowerCase()];
+  pool
+    .query(query, data)
+    .then((result) => {
+      if (result.rows.length) {
+        bcrypt.compare(password, result.rows[0].password, (err, response) => {
+          if (err) res.json(err);
+          if (response) {
+            const payload = {
+              userId: result.rows[0].user_id,
+
+              role: result.rows[0].role,
+            };
+            const options = { expiresIn: "1d" };
+            const secret = process.env.SECRET;
+            const token = jwt.sign(payload, secret, options);
+            if (token) {
+              return res.status(200).json({
+                success: true,
+                message: `Valid login credentials`,
+                token,
+                userId: result.rows[0].id,
+              });
+            } else {
+              throw Error;
+            }
+          } else {
+            res.status(403).json({
+              success: false,
+              message: `The email doesn’t exist or the password you’ve entered is incorrect`,
+            });
+          }
+        });
+      } else throw Error;
+    })
+    .catch((err) => {
+      res.status(403).json({
+        success: false,
+        message:
+          "The email doesn’t exist or the password you’ve entered is incorrect",
+        err,
+      });
+    });
+};
 const deleteUser = async (req, res) => {
   const { id } = req.params;
   const QUERY = ``;
