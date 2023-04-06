4d225f031f165d1d1b086230c0e45b78a4f96b52
AhmedAlSawalmeh <sawalmeh.ahmed@gmail.com>
2023-04-07 00:43:31 +0300

    answer the friend requst done

--- backend/controllers/friends.js
+++ backend/controllers/friends.js
@@ -34,7 +34,7 @@ const QUERY = `SELECT * FROM connection WHERE user_id=$1 `
 
 try{
  const response = pool.query(QUERY,VALUE)
- res.status(201).json({
+ res.status(200).json({
   success :true,
   Message:"Connection Status",
   connection:response.rows
@@ -51,8 +51,22 @@ try{
 const answerFriendRequest = (req,res)=>{
 const id = req.params.id
 const {status} = req.body
-const VALUE = []
-const QUERY = `UPDATE Connection SET status= `
+const VALUE = [status,id]
+const QUERY = `UPDATE Connection SET status=$1 WHERE user_id=$2 RETURNING * `
+try{
+  const response = pool.query(QUERY,VALUE)
+  res.status(203).json({
+   success :true,
+   Message:"Connection Response",
+   connection:response.rows
+  })
+ }catch(err){
+   res.status(500).json({
+     success: false,
+     message: "Server Error",
+     err: err.message
+   })
+ }
 }
 
 const deleteFriendRequest = (req,res)=>{
