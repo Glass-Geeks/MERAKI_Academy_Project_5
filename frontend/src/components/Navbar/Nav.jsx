4303d016160587e9eefb0c23564e1fb6d6a8dc22
mousaibrah <mousaKibrahim@gmail.com>
2023-04-10 01:12:04 +0300

    school-img

--- frontend/src/components/Navbar/Nav.jsx
+++ frontend/src/components/Navbar/Nav.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { NavBar } from "../Styled/Navbar.styled";
 import { Link } from "react-router-dom";
-const Nav = () => {
+const Nav = ({ links }) => {
+  console.log('links :>> ', links);
   return (
     <NavBar>
-      <Link className="links" to={"/"}>Home</Link>
-      <Link className="links" to={"/login"}>Login</Link>
+      <div className="home-logo">
+        <Link className="links" to={"/"}>
+          Home
+        </Link>
+      </div>
+      <div className="nav-links">
+        {links?.home}
+        </div>
     </NavBar>
   );
 };
