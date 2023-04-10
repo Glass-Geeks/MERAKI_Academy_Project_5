2efb430f8b69f1ff2003a7543b6720be19f905e4
AhmedAlSawalmeh <sawalmeh.ahmed@gmail.com>
2023-04-11 00:03:46 +0300

    Start with login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Err404 from "./components/Error/Err404";
 import MapContainer from "./components/Map/Map";
 import { Routes, Route } from "react-router-dom";
 import School from "./components/School/School";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MapContainer />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/school/:id" element={<School />} />
           <Route path="*" element={<Err404 />} />
         </Routes>
