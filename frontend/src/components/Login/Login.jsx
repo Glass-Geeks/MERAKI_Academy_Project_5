64fd6f9b51cb1727f653d6ec8d6042556bdc1280
AhmedAlSawalmeh <sawalmeh.ahmed@gmail.com>
2023-04-11 00:27:55 +0300

    almost done with login

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,85 @@
+import React, { useState} from "react";
+
+import "./Login.css";
+import { useDispatch,useSelector } from "react-redux";
+import {setLogin, setUserId, setLogout} from "../store/auth/index"
+import axios from "axios";
+
+
+
+
+
+const Login = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(false);
+  const dispatch = useDispatch()
+  const state = useSelector ((state)=>{
+    return {
+      auth:state.auth
+    }
+  })
+
+  
+
+  const login = async (e) => {
+    e.preventDefault();
+    const user = userData
+    try {
+      const result = await axios.post("http://localhost:5000/users/login", user);
+      if (result.data) {
+        setMessage("");
+        dispatch(setLogin(result.data.token))
+        dispatch(setUserId(result.data.userId))
+        
+      } else throw Error;
+    } catch (error) {
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Login, please try again");
+    }
+  };
+
+ 
+
+  return (
+    <>
+      <div className="Form">
+        <p className="Title">Login:</p>
+        <form onSubmit={login}>
+          <br />
+
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setUserData({...userData,email:e.target.value})}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setUserData({...userData,password:e.target.value})}
+          />
+          <br />
+          <button
+            onClick={(e) => {
+              login(e);
+            }}
+          >
+            Login
+          </button>
+        </form>
+
+        {status
+          ? message && <div className="SuccessMessage">{message}</div>
+          : message && <div className="ErrorMessage">{message}</div>}
+      </div>
+    </>
+  );
+};
+
+export default Login;
