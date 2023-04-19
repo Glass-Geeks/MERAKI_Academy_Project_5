dc3010ac896eb4be061415de3f585c14e185daa5
mousaibrah <mousaKibrahim@gmail.com>
2023-04-19 04:18:51 +0300

    connection-reducer

--- frontend/src/components/store/store.js
+++ frontend/src/components/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import schoolReducer from "./schools/index";
 import friendReducer from "./friends/index";
 import authReducer from "./auth/index";
+import connectionReducer from "./Connection";
 export default configureStore({
   // the reducer object is empty for now but after creating reducers we add them to this object
   reducer: {
     schools: schoolReducer,
     friends: friendReducer,
     auth: authReducer,
+    connection: connectionReducer,
   },
 });
